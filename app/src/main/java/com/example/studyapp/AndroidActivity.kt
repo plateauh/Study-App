07e05e57a96e3d736c1ecebcfd66cbb4be7b6e42
plateauh <70834431+plateauh@users.noreply.github.com>
2021-09-29 13:19:36 +0300

    Topics added, done

--- app/src/main/java/com/example/studyapp/AndroidActivity.kt
+++ app/src/main/java/com/example/studyapp/AndroidActivity.kt
@@ -38,7 +38,35 @@ class AndroidActivity : AppCompatActivity() {
 
     private fun setTopics(): ArrayList<Topic>{
         return arrayListOf(
-            Topic("Topic3", "Topic 3 is great", "Topic 3 details"),
-            Topic("Topic4", "Topic 4 is great", "Topic 4 details"))
+            Topic("Project Structure", "Important folders", "There is multiple folders in the project that serve different purposes:\n" +
+                    "\n" +
+                    "Gradle Scripts: Used to build .apk file for the app to be deployed. The developer can import dependencies using build.gradle file.\n" +
+                    "\n" +
+                    "Manifests: Contains AndroidManifest.xml which allows us to get permissions for wifi, gps… etc from user’s phone (and also to define activities and intent filters).\n" +
+                    "\n" +
+                    "Java: Has 3 subfolder with the same name as the package’s. The difference is that one of them is for app logic, the other two are for testing.\n" +
+                    "The app logic folder contains Kotlin files of activities including MainActivity.kt which is the entry point of the app.\n" +
+                    "\n" +
+                    "Resource: Holds various types of app resources including: \n" +
+                    "- drawable: Contains app’s icons.\n" +
+                    "- layout: Contains visual aspects of app (user interfaces). eg.: activity_main.xml.\n" +
+                    "- mipmap: Contains app’s icons with different sizes and configurations.\n" +
+                    "- values: Contains app’s values which are: colors.xml for colors values, strings.xml for string values, and theme subfolder that contains themes.xml files for both light and night modes."),
+
+            Topic("Design", "UI design", "Important views (so far):\n" +
+                    "- TextView: to display text.\n" +
+                    "- Plain Text: to input text.\n" +
+                    "- Button: to be pressed :D.\n" +
+                    "- LinearLayout: Horizontal to place elements next to each other (in a row). Vertical to place elements below each other (in a column).\n" +
+                    "\n" +
+                    "Important (and common) attributes:\n" +
+                    "- id: helps in handling the view in activity (logic) file.\n" +
+                    "- layout_width & layout_height: the width & height of the view. Their values can be: match_parent, wrap_content, or specific numeric value.\n" +
+                    "- text: text displayed in view."),
+
+            Topic("Logic", "To make the app interactive", "There is an override of onCreate function which sets the layout file as content view. onCreate function is the starting point of the activity.\n" +
+                    "Initialize view elements by using findViewById(R.id.view_ID) function. Make sure to do this after the call of setContentView().\n" +
+                    "To initiate the variable of view outside onCreate() function, use lateinit keyword to tell android that these variables will be initialized later in code.\n")
+        )
     }
 }
--- app/src/main/java/com/example/studyapp/KotlinActivity.kt
+++ app/src/main/java/com/example/studyapp/KotlinActivity.kt
@@ -38,7 +38,38 @@ class KotlinActivity : AppCompatActivity() {
 
     private fun setTopics(): ArrayList<Topic>{
         return arrayListOf(
-            Topic("Topic1", "Topic 1 is great", "Topic 1 details"),
-            Topic("Topic2", "Topic 2 is great", "Topic 2 details"))
+            Topic("Print", "Print to console in Kotlin", "Kotlin has 2 print methods:\n\n" +
+                    "- print(): the cursor will stay at the same line after execution.\n\n" +
+                    "- println(): the cursor will move to the next line after execution."),
+
+            Topic("val & var", "Variables declaration", "In Kotlin, there is 2 types of variables:\n" +
+                    "val: which is immutable (unchangeable value).\n\n" +
+                    "var: which is mutable (changeable value).\n\n" +
+                    "To define & initialize a variable without specifying the data type, follow this rule:\n\n" +
+                    "(val/var) variable_name = value\n\n" +
+                    "eg.: val message = “Hello World”"),
+
+            Topic("Data types", "Useful data types", "Kotlin has various data types, including but not limited to:\n\n" +
+                    "- Int\n" +
+                    "- Float\n" +
+                    "- Boolean\n" +
+                    "- String\n" +
+                    "- Char"),
+            Topic("String Manipulation", "Concatenation & Interpolation", "Concatenation: you can simply concat multiple strings by using +. eg.: “Hello ” + “World”.\n\n" +
+                    "Interpolation: you can embed a variable value in a string using \$. eg.: “Good Morning \$message”."),
+
+            Topic("If statements & When", "if(), if() else, &&, ||, and !", "In Kotlin, there is an equivalence of switch() which is when. It either can be:\n\n" +
+                    "- when {}: without a variable (a way to clean multiple ifs that are not necessarily related).\n\n" +
+                    "- when (variable) {}: with a variable to apply conditions on it."),
+
+            Topic("Loops", "for & while loops", "- for loop: eg. for (i in message), for (i in 0…10). in keyword is doing magic!\n\n" +
+                    "- while loop: don’t forget to control it! you don’t want to be stuck in an infinite loop!"),
+
+            Topic("Try/Catch", "An example", "try {\n\n" +
+                    "\t// Code blocks that might cause crashing\n\n" +
+                    "} catch (exception_variable_name: Exception_Type){\n\n" +
+                    "\t// What to do after crashing\n\n" +
+                    "}")
+        )
     }
 }
