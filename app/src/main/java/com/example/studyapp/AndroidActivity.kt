c2acc04028f9b987e9171fd8bf63795e5b25fe34
plateauh <70834431+plateauh@users.noreply.github.com>
2021-09-29 11:28:49 +0300

    navigating between activities done

---
+++ app/src/main/java/com/example/studyapp/AndroidActivity.kt
@@ -0,0 +1,29 @@
+package com.example.studyapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class AndroidActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_android)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.toMain -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- app/src/main/java/com/example/studyapp/MainActivity.kt
+++ app/src/main/java/com/example/studyapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.studyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+
+lateinit var kotlinButton: Button
+lateinit var androidButton: Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        kotlinButton = findViewById(R.id.kotlinBtn)
+        kotlinButton.setOnClickListener {
+            val intent = Intent(this, KotlinActivity::class.java)
+            startActivity(intent)
+        }
+
+        androidButton = findViewById(R.id.androidBtn)
+        androidButton.setOnClickListener {
+            val intent = Intent(this, AndroidActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
